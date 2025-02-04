6efcd94061074a5a18211bd919af316d15ea0085
vijay9837 <vkparjapati555@gmail.com>
2025-02-04 22:10:55 +0530

    modified html css js

---
+++ project5/script.js
@@ -0,0 +1,9 @@
+// Initialize Lenis
+const lenis = new Lenis({
+    autoRaf: true,
+    lerp:0.1
+  });
+  
+  // Listen for the scroll event and log the event data
+  lenis.on('scroll', (e) => {
+  });
